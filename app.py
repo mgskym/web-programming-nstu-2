2cb7acc5225488b1510bb66765eb99d7900f8522
Julia Ovchinnikova <julia.ovchhinnikova@gmail.com>
2023-09-16 12:35:51 +0700

    Стили

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ app = Flask(__name__)
 def example():
     lab_number = '2'
     name = 'Овчинникова Юля, Маглицкий Михаил'
-    return render_template('example.html', name=name, lab_number=lab_number)
+    cssPath = url_for('static', filename='lab1.css')
+    return render_template('example.html', name=name, lab_number=lab_number, cssPath=cssPath)
 
 @app.route("/menu")
 def menu():
