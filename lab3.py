1640d5bdd9f928d89dc2aecf76e1cffa0c78f949
Julia Ovchinnikova <julia.ovchhinnikova@gmail.com>
2023-09-30 18:03:12 +0700

    Валидация

--- lab3.py
+++ lab3.py
@@ -3,7 +3,10 @@ lab3 = Blueprint('lab3', __name__)
 
 @lab3.route('/lab3/form1')
 def form():
+    errors={}
     user = request.args.get('user')
+    if user == '':
+        errors['user'] = 'Заполните поле'
     age =  request.args.get('age')
     sex =  request.args.get('sex')
-    return render_template('lab3.html', user=user, age=age, sex=sex)
+    return render_template('lab3.html', user=user, age=age, sex=sex, errors=errors)
