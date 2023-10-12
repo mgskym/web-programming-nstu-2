c5f03fa01ffbc32724ecbd28448993a841e93f51
Michael Maglitscky <mihaillel@yandex.ru>
2023-10-12 19:23:18 +0700

    final

--- lab3.py
+++ lab3.py
@@ -8,7 +8,7 @@ def form():
     age = request.args.get('age')
     if user == '':
         errors['user'] = 'Заполните поле'
-    if str(age) == '':
+    if age == '':
         errors['age'] = 'Заполните поле'
     sex =  request.args.get('sex')
     return render_template('lab3.html', user=user, age=age, sex=sex, errors=errors)
@@ -22,6 +22,7 @@ def order():
 
 @lab3.route('/lab3/pay')
 def pay():
+    errors = {}
     price=0
     drink = request.args.get('drink')
     if drink == 'coffe':
@@ -40,10 +41,40 @@ def pay():
     card = request.args.get('card')
     name = request.args.get('name')
     cvv = request.args.get('cvv')
-    
-    return render_template('pay.html', price=price, card=card, name=name, cvv=cvv)
 
-@lab3.route('/lab3/success')
-def success():
-    return render_template('lab3pay.html')
+    if card == '':
+        errors['card'] = 'Заполните поле'
+    if name == '':
+        errors['name'] = 'Заполните поле'
+    if cvv == '':
+        errors['cvv'] = 'Заполните поле'
+    
+    return render_template('pay.html', price=price, card=card, name=name, cvv=cvv, errors=errors)
+
+@lab3.route('/lab3/ticket')
+def ticket():
+    errors={}
+    user = request.args.get('user')
+    age =  request.args.get('age')
+    ticket_type = request.args.get('ticket_type')
+    shelf_type = request.args.get('shelf_type')
+    baggage = request.args.get('baggage')
+    exit_point =  request.args.get('exit_point')
+    destination =  request.args.get('destination')
+    date =  request.args.get('date')
+
+    if user == '':
+         errors['user'] = 'Заполните поле'
+    if age == '':
+         errors['age'] = 'Заполните поле'
+    if exit_point == '':
+         errors['exit_point'] = 'Заполните поле'
+    if destination == '':
+         errors['destination'] = 'Заполните поле'
+    if date == '':
+         errors['date'] = 'Заполните поле'
+
+    return render_template('ticket.html',  user=user, age=age, ticket_type=ticket_type,
+    exit_point=exit_point, destination=destination, shelf_type=shelf_type,
+    baggage=baggage, date=date, errors=errors)
     
