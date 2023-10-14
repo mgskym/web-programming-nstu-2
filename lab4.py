5abcb42ece4c68d44a85908c92631f49e9ecf851
Julia Ovchinnikova <julia.ovchhinnikova@gmail.com>
2023-10-14 13:24:16 +0700

    1 задание не работает

--- lab4.py
+++ lab4.py
@@ -5,3 +5,19 @@ lab4 = Blueprint('lab4', __name__)
 @lab4.route('/lab4')
 def lab():
     return render_template('lab4.html')
+
+@lab4.route('/lab4/login', methods=['GET','POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('login.html')
+    
+    username=request.args.get('username')
+    password=request.args.get('password')
+    error=''
+    if username and password:
+        if username == 'alex' and password == '123':
+            return render_template('success.html')
+        else:
+            error = 'Неверные логин/пароль'
+    return render_template('login.html', error=error, username=username, password=password)
+    
