0f1a76c07e831754b7f2d7608ce5a7e15943e5fe
Julia Ovchinnikova <julia.ovchhinnikova@gmail.com>
2023-10-17 22:13:11 +0700

    Работает ура

--- lab4.py
+++ lab4.py
@@ -1,23 +1,28 @@
 from flask import Blueprint, render_template, request
-import math
-lab4 = Blueprint('lab4', __name__)
+lab4=Blueprint('lab4', __name__)
 
-@lab4.route('/lab4')
+@lab4.route('/lab4/')
 def lab():
     return render_template('lab4.html')
 
-@lab4.route('/lab4/login', methods=['GET','POST'])
+
+@lab4.route('/lab4/login', methods=['GET', 'POST'])
 def login():
-    if request.method == 'GET':
+    if request.method =='GET':
         return render_template('login.html')
     
-    username=request.args.get('username')
-    password=request.args.get('password')
-    error=''
-    if username and password:
-        if username == 'alex' and password == '123':
-            return render_template('success.html')
-        else:
-            error = 'Неверные логин/пароль'
-    return render_template('login.html', error=error, username=username, password=password)
+    username = request.form.get('username')
+    password = request.form.get('password')
+
+    if username == 'julia' and password == '1':
+        return render_template('success.html')
+    error = 'Неверные логин и/или пароль'
+    if username == '':
+        error='Вы не ввели логин'
+    if password == '':
+        error='Вы не ввели пароль'
+
+    return render_template('login.html',error=error,
+                           username=username, password=password)
+
     
