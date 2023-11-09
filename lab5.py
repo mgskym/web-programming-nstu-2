5425cb1acbe53f2f51544d4ac9d258fc55989bf4
KoH4eHbleI7porepbl <andreyka.minin.75@mail.ru>
2023-11-09 14:24:57 +0700

    jfjfjfj

--- lab5.py
+++ lab5.py
@@ -3,17 +3,33 @@ import psycopg2
 
 lab5 = Blueprint('lab5', __name__)
 
-@lab5.route("/lab5")
-def main():
+def dBConnect():
     conn = psycopg2.connect(
         host="127.0.0.1",
         database = "knowledge_base",
-        user = "Julia_knowledge_base",
-        password = "123"
-    ).encode('UNICODE')
+        user = "julia_knowledge_base",
+        password = "123",
+        port = 5432
+)
+    return conn;
 
+def dBClose(cursor,connection):
+    cursor.close()
+    connection.close()
+
+@lab5.route('/lab5')
+def main():
+    visibleUser = "Anon"
+    conn = psycopg2.connect(
+        host="127.0.0.1",
+        database = "knowledge_base",
+        user = "julia_knowledge_base",
+        password = "123",
+        port = 5432
+)
     cur = conn.cursor()
-    cur.execute("SELECT * FROM users;").encoding('latin-1')
+
+    cur.execute("SELECT * FROM users;")
 
     result = cur.fetchall()
 
@@ -22,4 +38,83 @@ def main():
 
     print(result)
 
-    return "go to console"
+    return render_template('lab5.html', username = visibleUser)
+
+@lab5.route('/lab5/users')
+def user():
+    conn = psycopg2.connect(
+        host="127.0.0.1",
+        database = "knowledge_base",
+        user = "julia_knowledge_base",
+        password = "123",
+        port = 5432
+)
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM users;")
+    result = cur.fetchall()
+    return render_template('lab5users.html', users=result)
+
+@lab5.route('/lab5/register', methods=['GET', 'POST'])
+
+def registerPage():
+
+    errors=[]
+
+
+ 
+
+    if request.method=='GET':
+
+        return render_template('register.html', errors=errors)
+
+   
+
+    username=request.form.get('username')
+
+    password=request.form.get('password')
+
+
+ 
+
+   
+
+    if not (username or password):
+
+        errors.append("please, fill the fields")
+
+        print(errors)
+
+        return render_template('register.html', errors=errors)
+
+
+ 
+
+    conn=dbConnect()
+
+    cur=conn.cursor()
+
+
+ 
+
+    cur.execute("SELECT username From users where username='{username}';")
+
+   
+
+    if cur.fetchone() is not None:
+
+        errors.append('the user already exists')
+
+        dbClose(cur,conn)
+
+        return render_template('register.html', errors=errors)
+
+   
+
+    cur.execute(f"INSERRT INTO users(username, password) VALUES ('{username}','{password}');")
+
+    dbClose(cur,conn)
+
+
+ 
+
+    return redirect('/lab5/login')
