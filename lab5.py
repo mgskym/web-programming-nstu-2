1b5a18f45c586f7070fd8ff6fc00c52da78c0ed9
Michael Maglitscky <mihaillel@yandex.ru>
2023-11-09 14:25:08 +0700

    xm

--- lab5.py
+++ lab5.py
@@ -1,19 +1,20 @@
 from flask import Blueprint, render_template, request, redirect
 import psycopg2
 
+
 lab5 = Blueprint('lab5', __name__)
 
 @lab5.route("/lab5")
 def main():
     conn = psycopg2.connect(
         host="127.0.0.1",
-        database = "knowledge_base",
-        user = "Julia_knowledge_base",
-        password = "123"
-    ).encode('UNICODE')
+        database = "knowledge_base_for_michael",
+        user = "Michael_knowledge_base",
+        password = "001914"
+    )
 
     cur = conn.cursor()
-    cur.execute("SELECT * FROM users;").encoding('latin-1')
+    cur.execute("SELECT * FROM users;")
 
     result = cur.fetchall()
 
@@ -23,3 +24,5 @@ def main():
     print(result)
 
     return "go to console"
+
+   
