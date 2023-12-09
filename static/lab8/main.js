bfac9a9c30839ec68355206e23707a17663290aa
Студент Факультета Бизнеса <student@FB.LOCAL>
2023-12-09 12:53:42 +0700

    таблица

--- static/lab8/main.js
+++ static/lab8/main.js
@@ -1,5 +1,5 @@
 function fillCourseList() {
-    fetch('lab8/api/courses')
+    fetch('api/courses')
     .then(function(data) {
         return data.json();
     })
@@ -9,9 +9,20 @@ function fillCourseList() {
         for(let i = 0; i < courses.length; i++) {
             tr = document.createElement('tr');
             let tdName = document.createElement('td');
+            tdName.innerText = courses[i].name;
             let tdVideos = document.createElement('td');
+            tdVideos.innerText = courses[i].videos;
             let tdPrice = document.createElement('td');
+            tdPrice.innerText = courses[i].price || 'бесплатно'; 
+            
+            let editButton = document.createElement('button');
+            editButton.innerText = 'редактировать';
+            let delButton = document.createElement('button');
+            delButton.innerText = 'удалить';
+
             let tdActions = document.createElement('td');
+            tdActions.append(editButton);
+            tdActions.append(delButton);
 
             tr.append(tdName);
             tr.append(tdVideos);
