759c4b99861f3e827331463bb3c95cf938fb1f76
Julia <julia.ovchhinnikova@gmail.com>
2023-12-22 13:01:03 +0700

    С новой годой

--- lab9.py
+++ lab9.py
@@ -1,12 +1,28 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 lab9= Blueprint('lab9', __name__)
 
-@lab9.route('/lab9')
+@lab9.route('/lab9', methods=['GET', 'POST'])
 def main():
-    return render_template('lab9/index.html')
+    if request.method =='GET':
+        return render_template('lab9/index.html')
+    if request.method == 'POST':
+        recipient_name = request.form.get("recipient_name")
+        recipient_gender = request.form.get("recipient_gender")
+        sender_name = request.form.get("sender_name")
+    
+        return render_template('lab9/postcard.html', recipient_name=recipient_name, recipient_gender=recipient_gender, sender_name=sender_name)
 
 
 @lab9.app_errorhandler(404)
 def not_found(e):
     return render_template('lab9/404.html'), 404
+
+@lab9.route('/lab9/500')
+def server_error():
+    raise Exception("Произошла некоторая ошибка")
+
+@lab9.app_errorhandler(Exception)
+def er500(e):
+   return render_template('lab9/500.html'), 500
+
